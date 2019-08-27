125a10cedef92bb06e602089da7864e1b82014ad
Afshin T. Darian <git@darian.af>
2019-08-27 10:29:34 +0100

    v0.0.1_py

---
+++ setup.py
@@ -0,0 +1,20 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="qualia",
+    version="0.0.1",
+    author="Afshin T. Darian",
+    author_email="qualia@darian.af",
+    description="qualia",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/ursiform/qualia",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Operating System :: OS Independent",
+    ],
+)
