3811f7e997a5b774ecfaebf1bf8f80d2d6fbc30f
Afshin T. Darian <git@darian.af>
2020-04-13 14:32:41 +0100

    nodes => graph

--- src/graph.ts
+++ src/graph.ts
@@ -15,14 +15,14 @@ export class Graph<T extends IObservableDisposable>
   }
 
   add(node: T, key = UUID.uuid4()) {
-    const { nodes } = this;
+    const { graph } = this;
 
-    if (nodes.has(key)) {
+    if (graph.has(key)) {
       console.warn(`Graph#add: ${key} already exists.`);
       return;
     }
 
-    nodes.set(key, { node, in: new Map(), out: new Map() });
+    graph.set(key, { node, in: new Map(), out: new Map() });
     node.disposed.connect(() => {
       this.remove(key);
     }, this);
@@ -37,28 +37,28 @@ export class Graph<T extends IObservableDisposable>
   }
 
   link(a: string, b: string) {
-    const { nodes } = this;
+    const { graph } = this;
 
-    if (!nodes.has(a)) {
+    if (!graph.has(a)) {
       throw new ReferenceError(`Graph#link: ${a} does not exist.`);
     }
-    if (!nodes.has(b)) {
+    if (!graph.has(b)) {
       throw new ReferenceError(`Graph#link: ${b} does not exist.`);
     }
 
-    nodes.get(a)!.out.set(b);
-    nodes.get(b)!.in.set(a);
+    graph.get(a)!.out.set(b);
+    graph.get(b)!.in.set(a);
   }
 
   remove(key: string) {
-    const { nodes } = this;
+    const { graph } = this;
 
-    if (!nodes.has(key)) {
+    if (!graph.has(key)) {
       console.warn(`Graph#remove: ${key} does not exist.`);
       return;
     }
 
-    const node = nodes.get(key)!;
+    const node = graph.get(key)!;
 
     node.in.forEach((_, peer) => {
       this.unlink(key, peer);
@@ -66,22 +66,22 @@ export class Graph<T extends IObservableDisposable>
     node.out.forEach((_, peer) => {
       this.unlink(key, peer);
     });
-    nodes.delete(key);
+    graph.delete(key);
     Signal.disconnectBetween(node, this);
   }
 
   unlink(a: string, b: string) {
-    const { nodes } = this;
+    const { graph } = this;
 
-    if (!nodes.has(a)) {
+    if (!graph.has(a)) {
       throw new ReferenceError(`Graph#unlink: ${a} does not exist.`);
     }
-    if (!nodes.has(b)) {
+    if (!graph.has(b)) {
       throw new ReferenceError(`Graph#unlink: ${b} does not exist.`);
     }
 
-    const one = nodes.get(a)!;
-    const two = nodes.get(b)!;
+    const one = graph.get(a)!;
+    const two = graph.get(b)!;
 
     one.in.delete(b);
     one.out.delete(b);
@@ -89,7 +89,7 @@ export class Graph<T extends IObservableDisposable>
     two.out.delete(a);
   }
 
-  protected nodes = new Map<
+  protected graph = new Map<
     string,
     {
       node: T;
