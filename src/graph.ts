75604e9a02303b736a495c38bc5b59d1e77c4b3a
Afshin T. Darian <git@darian.af>
2020-04-13 04:06:50 +0100

    Update Graph

--- src/graph.ts
+++ src/graph.ts
@@ -14,16 +14,20 @@ export class Graph<T extends IObservableDisposable>
     return this._isDisposed;
   }
 
-  dispose(): void {
+  add(node: T, id = UUID.uuid4()) {
+    if (this.data.has(id)) {
+      return;
+    }
+    this.data.set(id, node);
+    this.graph.set(id, Object.create(null));
+  }
+
+  dispose() {
     if (this._isDisposed) {
       return;
     }
     this._isDisposed = true;
     this._disposed.emit(undefined);
-  }
-
-  add(node: T, id = UUID.uuid4()) {
-    // Add a node.
   }
 
   link(a: string, b: string) {
@@ -33,14 +37,21 @@ export class Graph<T extends IObservableDisposable>
     if (!this.data.has(b)) {
       throw new ReferenceError(`Graph#link: ${b} does not exist.`);
     }
-    // Create a link.
+    this.graph.get(a)![b] = null;
   }
 
-  unlink(a: T, b: T): void {
-    // Remove an edge.
+  unlink(a: string, b: string) {
+    if (!this.data.has(a)) {
+      throw new ReferenceError(`Graph#unlink: ${a} does not exist.`);
+    }
+    if (!this.data.has(b)) {
+      throw new ReferenceError(`Graph#unlink: ${b} does not exist.`);
+    }
+    delete this.graph.get(a)![b];
   }
 
-  protected data =  new Map<string, string[]>();
+  protected data = new Map<string, T>();
+  protected graph = new Map<string, { [id: string]: null }>();
 
   private _disposed = new Signal<this, void>(this);
   private _isDisposed = false;
