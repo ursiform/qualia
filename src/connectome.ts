fded93c4288f502324b5966c0e77e6234b96aa94
Afshin T. Darian <git@darian.af>
2020-04-13 21:00:42 +0100

    Update example

--- src/connectome.ts
+++ src/connectome.ts
@@ -1,6 +1,17 @@
 // (c) Copyright Afshin T. Darian 2020. All Rights Reserved.
 
+import { IDisposable } from '@lumino/disposable';
 import { Graph } from './graph';
 import { Neuron } from './neuron';
 
-export class Connectome extends Graph<Neuron> {}
+export class Connectome implements IDisposable {
+  graph = new Graph<Neuron>();
+
+  get isDisposed(): boolean {
+    return this.graph.isDisposed;
+  }
+
+  dispose() {
+    this.graph.dispose();
+  }
+}
