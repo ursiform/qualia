42ce6e28ab7cb2e31464b07c27fc21dc1fad7471
Afshin T. Darian <git@darian.af>
2020-04-13 12:53:06 +0100

    Add example

--- example/src/index.ts
+++ example/src/index.ts
@@ -1,5 +1,5 @@
 // (c) Copyright Afshin T. Darian 2020. All Rights Reserved.
 
-import { Qualia } from '.';
+import { Qualia } from 'qualia';
 
 console.log(`Running qualia version ${Qualia.version}`);
