e0c7241b2637426c0f28cb081b3243a5c9b00f0b
Afshin T. Darian <git@darian.af>
2020-05-20 22:05:51 +0100

    Read version dynamically in qualia package

--- packages/qualia/src/qualia.ts
+++ packages/qualia/src/qualia.ts
@@ -1,5 +1,7 @@
 // (c) Copyright Afshin T. Darian 2020. All Rights Reserved.
 
+import * as qualia from '../package.json';
+
 export namespace Qualia {
-  export const version = '0.0.2';
+  export const version = qualia.version;
 }
